73ebfd98441e88a4b3f6ab4613b9f2fc252b6c5d
tradermohamed <mohamed@chameleontrading.com>
2023-09-13 16:13:31 -0400

    add 30k & 100k to slippage chart

--- components/home/charts/liquidity.tsx
+++ components/home/charts/liquidity.tsx
@@ -25,19 +25,21 @@ import { liquidity_by_coin } from '../../../constants/api';
 
 const REQUESTS = [liquidity_by_coin];
 
-export default function Liquidity(props: any) {
-  const isMobile = props.isMobile;
-
+export default function Liquidity() {
   const [formattedData0, setFormattedData0] = useState<any[]>([]);
   const [formattedData1000, setFormattedData1000] = useState<any[]>([]);
   const [formattedData10000, setFormattedData10000] = useState<any[]>([]);
+  const [formattedData30000, setFormattedData30000] = useState<any[]>([]);
+  const [formattedData100000, setFormattedData100000] = useState<any[]>([]);
 
   const [coinKeys, setCoinKeys] = useState<any[]>([]);
   const [coinKeys0, setCoinKeys0] = useState<any[]>([]);
   const [coinKeys1000, setCoinKeys1000] = useState<any[]>([]);
   const [coinKeys10000, setCoinKeys10000] = useState<any[]>([]);
+  const [coinKeys30000, setCoinKeys30000] = useState<any[]>([]);
+  const [coinKeys100000, setCoinKeys100000] = useState<any[]>([]);
 
-  const [dataMode, setDataMode] = useState<'0' | '1000' | '10000'>('0');
+  const [dataMode, setDataMode] = useState<'0' | '1000' | '10000'| '30000'| '100000'>('0');
   const [coinsSelected, setCoinsSelected] = useState<string[]>(initialTokensSelected);
 
   const [dataLiqudity, loadingLiqudity, errorLiqudity] = useRequest(REQUESTS[0], [], 'chart_data');
@@ -61,6 +63,16 @@ export default function Liquidity(props: any) {
         event: () => setDataMode('10000'),
         active: dataMode === '10000',
       },
+      {
+        text: '$30k',
+        event: () => setDataMode('30000'),
+        active: dataMode === '30000',
+      },
+      {
+        text: '$100k',
+        event: () => setDataMode('100000'),
+        active: dataMode === '100000',
+      },
     ],
   };
 
@@ -69,6 +81,8 @@ export default function Liquidity(props: any) {
       median_slippage_0: number;
       median_slippage_1000: number;
       median_slippage_10000: number;
+      median_slippage_30000: number;
+      median_slippage_100000: number;
       time: string;
     }[];
   };
@@ -77,6 +91,8 @@ export default function Liquidity(props: any) {
     median_slippage_0: { time: Date; [key: string]: number | Date | string }[];
     median_slippage_1000: { time: Date; [key: string]: number | Date | string }[];
     median_slippage_10000: { time: Date; [key: string]: number | Date | string }[];
+    median_slippage_30000: { time: Date; [key: string]: number | Date | string }[];
+    median_slippage_100000: { time: Date; [key: string]: number | Date | string }[];
   };
 
   const extractCoins = (data: InputData): string[] => {
@@ -106,6 +122,14 @@ export default function Liquidity(props: any) {
       string,
       { time: Date; [key: string]: number | Date | string }
     >();
+    const median_slippage_30000 = new Map<
+    string,
+    { time: Date; [key: string]: number | Date | string }
+  >();
+  const median_slippage_100000 = new Map<
+  string,
+  { time: Date; [key: string]: number | Date | string }
+>();
 
     for (let key in filteredData) {
       if (!filteredData[key]) {
@@ -117,19 +141,29 @@ export default function Liquidity(props: any) {
           median_slippage_0: val_0,
           median_slippage_1000: val_1000,
           median_slippage_10000: val_10000,
+          median_slippage_30000: val_30000,
+          median_slippage_100000: val_100000,
+
         } = record;
 
         const map0 = median_slippage_0.get(time) || { time: new Date(time), unit: '%' };
         const map1000 = median_slippage_1000.get(time) || { time: new Date(time), unit: '%' };
         const map10000 = median_slippage_10000.get(time) || { time: new Date(time), unit: '%' };
+        const map30000 = median_slippage_30000.get(time) || { time: new Date(time), unit: '%' };
+        const map100000 = median_slippage_100000.get(time) || { time: new Date(time), unit: '%' };
 
         map0[key] = val_0 * 100;
         map1000[key] = val_1000 * 100;
         map10000[key] = val_10000 * 100;
+        map30000[key] = val_30000 * 100;
+        map100000[key] = val_100000 * 100;
 
         median_slippage_0.set(time, map0);
         median_slippage_1000.set(time, map1000);
         median_slippage_10000.set(time, map10000);
+        median_slippage_30000.set(time, map30000);
+        median_slippage_100000.set(time, map100000);
+
       });
     }
 
@@ -147,6 +181,12 @@ export default function Liquidity(props: any) {
       median_slippage_10000: Array.from(median_slippage_10000.values()).sort((a, b) =>
         sortByDate(a.time, b.time)
       ),
+      median_slippage_30000: Array.from(median_slippage_30000.values()).sort((a, b) =>
+      sortByDate(a.time, b.time)
+    ),
+    median_slippage_100000: Array.from(median_slippage_100000.values()).sort((a, b) =>
+    sortByDate(a.time, b.time)
+  ),
     };
   };
 
@@ -171,13 +211,20 @@ export default function Liquidity(props: any) {
     setFormattedData0(formattedData.median_slippage_0);
     setFormattedData1000(formattedData.median_slippage_1000);
     setFormattedData10000(formattedData.median_slippage_10000);
+    setFormattedData30000(formattedData.median_slippage_30000);
+    setFormattedData100000(formattedData.median_slippage_100000);
+
     const formattedUniqueCoinKeys0 = extractUniqueCoins(formattedData.median_slippage_0);
     const formattedUniqueCoinKeys1000 = extractUniqueCoins(formattedData.median_slippage_1000);
     const formattedUniqueCoinKeys10000 = extractUniqueCoins(formattedData.median_slippage_10000);
+    const formattedUniqueCoinKeys30000 = extractUniqueCoins(formattedData.median_slippage_30000);
+    const formattedUniqueCoinKeys100000 = extractUniqueCoins(formattedData.median_slippage_100000);
 
     setCoinKeys0(formattedUniqueCoinKeys0);
     setCoinKeys1000(formattedUniqueCoinKeys1000);
     setCoinKeys10000(formattedUniqueCoinKeys10000);
+    setCoinKeys30000(formattedUniqueCoinKeys30000);
+    setCoinKeys100000(formattedUniqueCoinKeys100000);
   };
 
   useEffect(() => {
@@ -186,15 +233,30 @@ export default function Liquidity(props: any) {
     }
   }, [loading]);
 
-  const chartData =
-    dataMode === '0'
-      ? formattedData0
-      : dataMode === '1000'
-      ? formattedData1000
-      : formattedData10000;
-
-  const chartDataCoinKeys =
-    dataMode === '0' ? coinKeys0 : dataMode === '1000' ? coinKeys1000 : coinKeys10000;
+  let chartData;
+  let chartDataCoinKeys;
+  switch (dataMode) {
+    case '0':
+      chartData = formattedData0;
+      chartDataCoinKeys = coinKeys0;
+      break;
+    case '1000':
+      chartData = formattedData1000;
+      chartDataCoinKeys = coinKeys1000;
+      break;
+    case '10000':
+      chartData = formattedData10000;
+      chartDataCoinKeys = coinKeys10000;
+      break;
+    case '30000':
+      chartData = formattedData30000;
+      chartDataCoinKeys = coinKeys30000;
+      break;
+    case '100000':
+      chartData = formattedData100000;
+      chartDataCoinKeys = coinKeys100000;
+      break;
+  }
 
   const coinSelectors = createCoinSelectors(coinKeys, coinsSelected, setCoinsSelected, formatData);
 
